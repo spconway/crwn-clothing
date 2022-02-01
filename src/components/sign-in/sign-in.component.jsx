879213f3ef47d29820e3f301f0a2e2c95210f412
Stephen Conway <stephen.conway87@yahoo.com>
2022-01-31 21:49:34 -0500

    finishing sign in handleSubmit method

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 import FormInput from '../form-input/form-input.component';
 import CustomButton from '../custom-button/custom-button.component';
 
-import { signInWithGoogle } from '../../firebase/firebase.utils';
+import { auth, signInWithGoogle } from '../../firebase/firebase.utils';
 
 import './sign-in.styles.scss';
 
@@ -17,8 +17,17 @@ class SignIn extends React.Component {
 		};
 	}
 
-	handleSubmit = (event) => {
+	handleSubmit = async (event) => {
 		event.preventDefault();
+
+		const { email, password } = this.state;
+
+		try {
+			await auth.signInWithEmailAndPassword(email, password);
+			this.setState({ email: '', password: '' });
+		} catch (error) {
+			console.log(error);
+		}
 
 		this.setState({ email: '', password: '' });
 	};
@@ -31,29 +40,29 @@ class SignIn extends React.Component {
 
 	render() {
 		return (
-			<div className="sign-in">
+			<div className='sign-in'>
 				<h2>I already have an account</h2>
 				<span>Sign in with your email and password</span>
 
 				<form onSubmit={this.handleSubmit}>
 					<FormInput
-						name="email"
-						type="email"
-						label="email"
+						name='email'
+						type='email'
+						label='email'
 						value={this.state.email}
 						handleChange={this.handleChange}
 						required
 					/>
 					<FormInput
-						name="password"
-						type="password"
-						label="password"
+						name='password'
+						type='password'
+						label='password'
 						value={this.state.password}
 						handleChange={this.handleChange}
 						required
 					/>
-					<div className="buttons">
-						<CustomButton type="submit">Sign in</CustomButton>
+					<div className='buttons'>
+						<CustomButton type='submit'>Sign in</CustomButton>
 						<CustomButton onClick={signInWithGoogle} isGoogleSignIn>
 							Sign in with google
 						</CustomButton>
