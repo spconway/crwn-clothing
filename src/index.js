78696d344f70c158c19908e70427b56824225c3c
Stephen Conway <stephen.conway87@yahoo.com>
2022-01-31 23:24:48 -0500

    added redux and implemented userReducer and userActions to header and app components

--- src/index.js
+++ src/index.js
@@ -1,19 +1,24 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { BrowserRouter } from "react-router-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
 
-import reportWebVitals from "./reportWebVitals";
+import reportWebVitals from './reportWebVitals';
 
-import "./index.css";
-import App from "./App";
+import './index.css';
+import App from './App';
+
+import store from './redux/store';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>,
-  document.getElementById("root")
+	<React.StrictMode>
+		<Provider store={store}>
+			<BrowserRouter>
+				<App />
+			</BrowserRouter>
+		</Provider>
+	</React.StrictMode>,
+	document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
